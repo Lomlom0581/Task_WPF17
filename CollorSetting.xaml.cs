ff0ca9278eb2ec8772b326f2138cb18bb12cda9d
Антон Еремеев <kzanton_81@mail.ru>
2022-02-14 00:01:00 +0300

    Добавьте файлы проекта.

---
+++ CollorSetting.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_WPF17
+{
+    /// <summary>
+    /// Логика взаимодействия для CollorSetting.xaml
+    /// </summary>
+    public partial class CollorSetting : UserControl
+    {
+        public CollorSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void Color_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ColorS.Fill = new SolidColorBrush(Color.FromRgb((byte)redS.Value,
+                (byte)greenS.Value, (byte)blueS.Value));
+        }
+
+        public Brush GetColor
+        {
+            get
+            {
+                return ColorS.Fill;
+            }
+        }
+    }
+}
